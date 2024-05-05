e215821d81e27102d1b7b785f0e94fbdd179294e
JCSmith23467 <jcs3490@yahoo.com>
2024-05-05 17:36:41 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Justin Smith
+    File Name: script.js
+    Date: Oct 30, 2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+	$('.hero').height($(window).height());
+});
